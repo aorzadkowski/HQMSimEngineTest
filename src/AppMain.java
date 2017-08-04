c563d9169094a5ca30b49ee1c51727c3c555b07e
Adam Rzadkowski <aorzadkowski@gmail.com>
2017-08-04 00:53:07 -0500

    Added more teams and OT support
    Supports overtime now.

--- src/AppMain.java
+++ src/AppMain.java
@@ -24,12 +24,56 @@ public class AppMain {
 					new Player("KS Otto", Role.OFFENSIVE_DMAN, 16, 19, 15, 18, 19, 19, 16, 19, 20),
 					new Player("Dman Jerry", Role.DEFENSIVE_DMAN, 15, 15, 14, 15, 12, 15, 17, 17, 18));
 		
-		System.out.println("Teams are:");
-		System.out.println("\t" + team1 + "\n\t" + team2);
-		System.out.println("Team1 Final Score: \tTeam2 Final Score:");
-		for (int i = 0; i < 100; i++) {
-			System.out.print("Game " + (i+1)+ "\t");
-			SimEngine.getBoxScore(team1, team2);
+		Team bos = new Team("Boston Bruins",
+				new Player("Lucic", Role.SNIPER, 15, 16, 19, 19, 19, 19, 19, 19, 20),
+				new Player("Teemu Salami", Role.DANGLER, 12, 13, 15, 17, 16, 17, 17, 17, 18),
+				new Player("KS Otto", Role.OFFENSIVE_DMAN, 16, 19, 15, 18, 19, 19, 16, 16, 20),
+				new Player("Tidge", Role.DEFENSIVE_DMAN, 14, 15, 15, 15, 12, 15, 17, 17, 16)
+				);
+		
+		Team nyr = new Team("New York Rangers",
+				new Player("Dick Mcbutts", Role.SNIPER, 13, 15, 18, 18, 18, 18, 19, 19, 17),
+				new Player("tb", Role.PLAYMAKER, 16, 15, 14, 15, 15, 16, 17, 17, 16),
+				new Player("TaZeR", Role.DEFENSIVE_DMAN, 14, 16, 15, 15, 14, 16, 18, 18, 15),
+				new Player("Kapanen", Role.OFFENSIVE_DMAN, 14, 14, 11, 14, 13, 15, 13, 13, 15)
+				);
+		
+		Team chi = new Team("Chicago Blackhawks",
+				new Player("Dyaloreax", Role.SNIPER, 10, 15, 20, 20, 18, 17, 19, 19, 20),
+				new Player("Tallmidget", Role.TWO_WAY_FORWARD, 17, 15, 13, 14, 16, 13, 16, 16, 16),
+				new Player("BeeGeePi", Role.OFFENSIVE_DMAN, 17, 17, 15, 16, 16, 17, 18, 18, 20),
+				new Player("FatSquirrel", Role.OFFENSIVE_DMAN, 13, 13, 11, 13, 14, 14, 12, 12, 16)
+				);
+		
+		Team tor = new Team("Toronto Maple Leafs",
+				new Player("NHLKilla", Role.PLAYMAKER, 12, 13, 13, 14, 14, 14, 15, 15, 15),
+				new Player("Gabe", Role.DANGLER, 16, 15, 15, 16, 19, 18, 18, 18, 18),
+				new Player("xParabolax", Role.OFFENSIVE_DMAN, 14, 15, 12, 16, 16, 15, 14, 14, 17),
+				new Player("Jabba", Role.OFFENSIVE_DMAN,13, 14, 12, 15, 16, 14, 13, 13, 14)
+				);
+		
+		Team nsh = new Team("Nashville Predators",
+				new Player("CrabInATree", Role.PLAYMAKER, 19, 19, 17, 16, 18, 16, 19, 19, 20),
+				new Player("Tony Flow", Role.TWO_WAY_FORWARD, 13, 15, 14, 15, 17, 15, 16, 16, 20),
+				new Player("Zam", Role.OFFENSIVE_DMAN, 17, 18, 17, 16, 17, 17, 17, 17, 19),
+				new Player("Dman Jerry", Role.DEFENSIVE_DMAN, 15, 15, 14, 15, 12, 15, 17, 17, 18)
+				);
+		
+		Team win = new Team("Winnipeg Jets",
+				new Player("guy la floor", Role.TWO_WAY_FORWARD, 14, 15, 15, 17, 17, 17, 16, 16, 20),
+				new Player("Drag", Role.DANGLER, 13, 14, 14, 16, 16, 16, 16, 16, 17),
+				new Player("Dalfan", Role.OFFENSIVE_DMAN, 16, 18, 16, 17, 17, 16, 18, 18, 16),
+				new Player("Icey", Role.OFFENSIVE_DMAN, 15, 16, 14, 15, 14, 16, 16, 16, 17)
+				);
+		
+		Team[] lhlTeams = {bos, nyr, chi, tor, nsh, win};
+		
+		int gameCount = 1000;
+		
+		//System.out.println("Team1\tTeam2");
+		for (int i = 0; i < gameCount; i++) {
+			//System.out.println("Game " + (i+1));
+			SimEngine.getBoxScore(nsh, bos);
 		}
 	}
 
--- src/SimEngine.java
+++ src/SimEngine.java
@@ -8,17 +8,95 @@ public class SimEngine {
 		/*
 		System.out.print("Box scores:\t\t\tP1\tP2\tP3\tF\nTeam1: \t\t\t");
 		*/
-		int[] period1 = getNetScoreInPeriod(team1, team2);
-		int[] period2 = getNetScoreInPeriod(team1, team2);
-		int[] period3 = getNetScoreInPeriod(team1, team2);
+		int[][] boxScore = new int[3][2];
+		
+		boxScore[0] = getNetScoreInPeriod(team1, team2);
+		boxScore[1] = getNetScoreInPeriod(team1, team2);
+		boxScore[2] = getNetScoreInPeriod(team1, team2);
 		/*
 		System.out.println(period1[0] + "\t" + period2[0] + "\t" + period3[0] + "\t" + (period1[0] + period2[0] + period3[0]));
 		System.out.println("Team2: \t\t\t" + period1[1] + "\t" + period2[1] + "\t" + period3[1]+ "\t" + (period1[1] + period2[1] + period3[1]));
 		*/
 		
-		int team1Score = (period1[0]+period2[0]+period3[0]);
-		int team2Score = (period1[1]+period2[1]+period3[1]);
-		System.out.println(team1Score + "\t" + team2Score + "\t" + (team1Score > team2Score ? "1" : "0"));
+		int team1Score = (boxScore[0][0] + boxScore[1][0] + boxScore[2][0]);
+		int team2Score = (boxScore[0][1] + boxScore[1][1] + boxScore[2][1]);
+		
+		
+		if (team1Score == team2Score) {
+			int team1OTScore = 0;
+			int team2OTScore = 0;
+			int otCount = 0;
+			
+			while (team1OTScore == team2OTScore) {
+				int[] otPeriod = getNetScoreInPeriod(team1, team2);
+			
+				team1OTScore = otPeriod[0];
+				team2OTScore = otPeriod[1];
+				
+				if (team1OTScore > team2OTScore) {
+					team1OTScore = 1;
+					team2OTScore = 0;
+				}
+				else if (team2OTScore > team1OTScore) {
+					team2OTScore = 1;
+					team1OTScore = 0;
+				} 
+				else if (team1OTScore == team2OTScore && team1OTScore != 0) {
+					team1OTScore = 0;
+					team2OTScore = 0;
+				}
+				otCount += 1;
+			}
+			
+			int[][] newBoxScore = new int[3 + otCount][2];
+			
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 2; j++) {
+					newBoxScore[i][j] = boxScore[i][j];
+				}
+			}
+			
+			for (int i = 3; i < 3 + otCount; i++) {
+				for (int j = 0; j < 2; j++) {
+					newBoxScore[i][j] = 0;
+				}
+			}
+			
+			newBoxScore[2+otCount][0] = team1OTScore;
+			newBoxScore[2+otCount][1] = team2OTScore;
+			
+			boxScore = newBoxScore;
+		}
+		int team1FinalScore = 0;
+		int team2FinalScore = 0;
+		
+		String team1OutputLine = team1.teamName + "\t";
+		String team2OutputLine = team2.teamName + "\t";
+
+		for (int i = 0; i < boxScore.length; i++) {
+			team1OutputLine += boxScore[i][0] + "\t";
+			team2OutputLine += boxScore[i][1] + "\t";
+			
+			team1FinalScore += boxScore[i][0];
+			team2FinalScore += boxScore[i][1];
+		}
+		
+		/////////////
+		team1OutputLine += team1FinalScore;
+		team2OutputLine += team2FinalScore;
+		
+		int headerLength = boxScore.length;
+		String headerLine = "Team";
+		
+		for (int i = 0; i < headerLength; i++) {
+			headerLine += "\t" + (i <= 2 ? "P" + (i + 1) : "OT" + (i -2));
+		}
+		System.out.println(headerLine + "\tF");
+		System.out.println(team1OutputLine);
+		System.out.println(team2OutputLine);
+		//////////////
+		
+		//System.out.println(team1FinalScore + "\t" + team2FinalScore + "\t" + (team1FinalScore > team2FinalScore ? "1" : "0"));
 	}
 	
 	private static int[] getNetScoreInPeriod(Team team1, Team team2) {
@@ -48,7 +126,8 @@ public class SimEngine {
 	}
 	
 	private static int getScoreInPeriod(Team team, int score) {
-		double probability = team.oRatio - ((0.01 + team.inverseBRatio) * team.oRatio * (score * score));
+		//double probability = team.oRatio - ((0.01 + team.inverseBRatio) * team.oRatio * (score * score));
+		double probability = team.oRatio - ((0.01 + team.inverseBRatio) * team.oRatio * (score));
 		if (Math.random() <= probability) {
 			return 1 + getScoreInPeriod(team, score + 1);
 		}
@@ -66,7 +145,8 @@ public class SimEngine {
 	}
 	
 	private static int getBlocksInPeriod(Team team, int score) {
-		double probability = team.dRatio - ((0.01 + team.inverseBRatio) * team.dRatio * (score * score));
+		//double probability = team.dRatio - ((0.01 + team.inverseBRatio) * team.dRatio * (score * score));
+		double probability = team.dRatio - ((0.01 + team.inverseBRatio) * team.dRatio * (score));
 		if (Math.random() <= probability) {
 			return (-1) - getBlocksInPeriod(team, score + 1);
 		}
